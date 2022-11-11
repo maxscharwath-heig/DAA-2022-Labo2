c13a66675aa90c4661fc8640fbeee2cba5624ed3
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-11 09:51:52 +0100

    First groups

--- app/src/main/java/ch/heigvd/daa_labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa_labo2/MainActivity.kt
@@ -3,8 +3,10 @@ package ch.heigvd.daa_labo2
 import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.Group
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
 import java.util.*
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnDatePicker: ImageButton
     private lateinit var inputDate: EditText
 
+    private lateinit var studentInputsGroup: Group
+    private lateinit var workerInputsGroup: Group
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         btnDatePicker = findViewById(R.id.date_picker_actions)
         inputDate = findViewById(R.id.main_base_birthdate_edit)
 
+
+        // Hide one section just for testing
+        studentInputsGroup = findViewById(R.id.student_specific_group)
+        workerInputsGroup = findViewById(R.id.worker_specific_group)
+
+        // workerInputsGroup.visibility = VISIBLE
+        // studentInputsGroup.visibility = GONE
+        // TODO: show / hide according to radio
 
         val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         btnDatePicker.setOnClickListener {
